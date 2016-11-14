64f96dda5c93847fcd9f7ea87ddb3c40413155d9
Kevin Lau <kelau1993@gmail.com>
2016-11-13 18:00:59 -0800

    Add toast on scanner result

--- app/src/main/java/com/codepath/shopmyself/MainActivity.java
+++ app/src/main/java/com/codepath/shopmyself/MainActivity.java
@@ -6,16 +6,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.crashlytics.android.Crashlytics;
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,5 +74,27 @@ public class MainActivity extends AppCompatActivity {
         integrator.setOrientationLocked(false)
                   .setCaptureActivity(CustomScannerActivity.class)
                   .initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        IntentResult result =
+                IntentIntegrator
+                .parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Log.d(TAG, "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d(TAG, "Scanned");
+                Toast.makeText(this, "Scanned: " + result.getContents(),
+                               Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // This is important, otherwise the result will not be passed to the
+            // fragment.
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
