17dc415361a6670cba48c45310d1186708d5d63c
palexis3 <alexispatrick77@gmail.com>
2016-11-27 13:00:46 -0800

    removed unnecessary user model

--- app/src/main/java/com/codepath/shopmyself/Models/User.java
+++ app/src/main/java/com/codepath/shopmyself/Models/User.java
@@ -3,8 +3,15 @@ package com.codepath.shopmyself.Models;
 
 public class User {
 
-    private String userId;
-    private Items items;
+    private String email;
+    private String password;
 
+    public User() {
+        //default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
 
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
