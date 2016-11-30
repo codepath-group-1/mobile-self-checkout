bc1fc722dc017de171dc491adaf36fe8bc4b5ad6
palexis3 <alexispatrick77@gmail.com>
2016-11-29 23:30:15 -0800

    Added navigation bottom drawer

--- app/src/main/java/com/codepath/shopmyself/activities/MainActivity.java
+++ app/src/main/java/com/codepath/shopmyself/activities/MainActivity.java
@@ -2,6 +2,8 @@ package com.codepath.shopmyself.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -35,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     private String mUserId;
 
+    private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,6 +58,34 @@ public class MainActivity extends AppCompatActivity {
             loadLogInView();
         } else {
             mUserId = mFirebaseUser.getUid();
+
+            //Once user is logged in then, we can have
+            //bottom navigation view to load up the selected fragment.
+            //Scanner is the default fragment
+            bottomNavigationView = (BottomNavigationView)
+                    findViewById(R.id.bottom_navigation);
+
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.action_scan:
+
+                                    break;
+                                case R.id.action_cart:
+
+                                    break;
+                                case R.id.action_wish_list:
+
+                                    break;
+                                case R.id.action_history:
+
+                                    break;
+                            }
+                            return false;
+                        }
+                    });
         }
 
     }
