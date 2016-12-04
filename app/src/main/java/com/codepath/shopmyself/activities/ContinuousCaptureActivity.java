52c5967cbfcc8be01e636a2e1e325ee140f49a4c
Kevin Lau <kelau1993@gmail.com>
2016-12-04 01:54:16 -0800

    Change barcode scan to continuous scan
    This allows starting the details activity from the scanner and not
    having to go back to the activity that started the barcode scan
    activity.

---
+++ app/src/main/java/com/codepath/shopmyself/activities/ContinuousCaptureActivity.java
@@ -0,0 +1,163 @@
+package com.codepath.shopmyself.activities;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codepath.shopmyself.R;
+import com.google.zxing.ResultPoint;
+import com.google.zxing.client.android.BeepManager;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+
+import java.util.List;
+
+/**
+ * This performs continuous scanning. It does not return to calling activity
+ * after a scan.
+ */
+public class ContinuousCaptureActivity
+extends AppCompatActivity
+implements DecoratedBarcodeView.TorchListener {
+
+    private static final String TAG = ContinuousCaptureActivity.class.getSimpleName();
+    private DecoratedBarcodeView barcodeView;
+    private BeepManager beepManager;
+    private MenuItem switchFlashlightMenuItem;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            barcodeView.pause();
+            beepManager.playBeepSoundAndVibrate();
+
+            String scannedMessage = "Scanned: " + result.getText();
+            Log.d(TAG, scannedMessage);
+            Toast.makeText(ContinuousCaptureActivity.this, scannedMessage,
+                           Toast.LENGTH_LONG).show();
+            long upc = Long.valueOf(result.getText());
+            launchDetailActivity(upc);
+
+            barcodeView.resume();
+        }
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    public void launchDetailActivity(long upc) {
+        Intent productDetailsIntent
+            = new Intent(this, ProductDetailsActivity.class);
+        productDetailsIntent.putExtra("upc", upc);
+        startActivity(productDetailsIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.continuous_scan);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        barcodeView
+            = (DecoratedBarcodeView)findViewById(R.id.zxing_barcode_scanner);
+        barcodeView.setTorchListener(this);
+        barcodeView.initializeFromIntent(getIntent());
+        barcodeView.decodeContinuous(callback);
+
+        beepManager = new BeepManager(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_continuous_capture, menu);
+
+        switchFlashlightMenuItem = menu.findItem(R.id.action_flashlight);
+        if (!hasFlash()) {
+            switchFlashlightMenuItem.setVisible(false);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_flashlight) {
+            switchFlashlight(null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    public void triggerScan(View view) {
+        barcodeView.decodeSingle(callback);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * Check if the device's camera has a Flashlight.
+     * @return true if there is Flashlight, otherwise false.
+     */
+    private boolean hasFlash() {
+        return getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+    }
+
+    public void switchFlashlight(View view) {
+        if (getString(R.string.turn_on_flashlight).equals(switchFlashlightMenuItem.getTitle())) {
+            barcodeView.setTorchOn();
+        } else {
+            barcodeView.setTorchOff();
+        }
+    }
+
+    @Override
+    public void onTorchOn() {
+        switchFlashlightMenuItem.setTitle(R.string.turn_off_flashlight);
+        switchFlashlightMenuItem.setIcon(R.drawable.ic_action_turn_off_flashlight);
+    }
+
+    @Override
+    public void onTorchOff() {
+        switchFlashlightMenuItem.setTitle(R.string.turn_on_flashlight);
+        switchFlashlightMenuItem.setIcon(R.drawable.ic_action_turn_on_flashlight);
+    }
+}
--- app/src/main/java/com/codepath/shopmyself/fragments/ScannerFragment.java
+++ app/src/main/java/com/codepath/shopmyself/fragments/ScannerFragment.java
@@ -1,28 +1,22 @@
 package com.codepath.shopmyself.fragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.codepath.shopmyself.R;
-import com.codepath.shopmyself.activities.CustomScannerActivity;
-import com.codepath.shopmyself.activities.ProductDetailsActivity;
+import com.codepath.shopmyself.activities.ContinuousCaptureActivity;
 import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
 
 public class ScannerFragment extends Fragment {
 
     protected static final String TAG = ScannerFragment.class
                                                        .getSimpleName();
 
-    private long upc;
     private Button btnScanBarcode;
 
     @Override
@@ -51,43 +45,8 @@ public class ScannerFragment extends Fragment {
         IntentIntegrator integrator
             = IntentIntegrator.forSupportFragment(this);
         integrator.setOrientationLocked(false)
-                  .setCaptureActivity(CustomScannerActivity.class)
-                  .setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES)
-                  .initiateScan();
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        IntentResult result
-            = IntentIntegrator
-              .parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Log.d(TAG, "Cancelled scan");
-                Toast.makeText(getActivity(), "Cancelled",
-                               Toast.LENGTH_LONG).show();
-            } else {
-                String scannedMessage = "Scanned: " + result.getContents();
-                Log.d(TAG, scannedMessage);
-                Toast.makeText(getActivity(), scannedMessage,
-                               Toast.LENGTH_LONG).show();
-                upc = Long.valueOf(result.getContents());
-                launchDetailActivity();
-            }
-        } else {
-            Log.d(TAG, "Scan ERROR");
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    public void launchDetailActivity() {
-        Intent i = new Intent(getActivity(), ProductDetailsActivity.class);
-        i.putExtra("upc", upc);
-        startActivity(i);
-
+                  .setCaptureActivity(ContinuousCaptureActivity.class)
+                  .setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        startActivity(integrator.createScanIntent());
     }
 }
-
-
