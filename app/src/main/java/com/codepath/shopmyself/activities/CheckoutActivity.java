4bb77fdd2dd4534ba118299150e5d50441b42e60
palexis3 <alexispatrick77@gmail.com>
2016-12-06 18:21:28 -0800

    Getting list of items in checkout activity

--- app/src/main/java/com/codepath/shopmyself/activities/CheckoutActivity.java
+++ app/src/main/java/com/codepath/shopmyself/activities/CheckoutActivity.java
@@ -1,15 +1,23 @@
 package com.codepath.shopmyself.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.codepath.shopmyself.R;
+import com.codepath.shopmyself.models.Item;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
 
 public class CheckoutActivity extends AppCompatActivity {
 
+    ArrayList<Item> itemArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        itemArrayList = Parcels.unwrap(getIntent().getParcelableExtra("itemList"));
+        
     }
 }
