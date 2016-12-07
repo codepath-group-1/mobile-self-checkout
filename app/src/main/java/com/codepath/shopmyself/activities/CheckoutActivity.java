87841242a82d5d17860d37514a62b7deeb1df3bb
palexis3 <alexispatrick77@gmail.com>
2016-12-07 15:23:56 -0800

    Added proper checkout xml

--- app/src/main/java/com/codepath/shopmyself/activities/CheckoutActivity.java
+++ app/src/main/java/com/codepath/shopmyself/activities/CheckoutActivity.java
@@ -3,15 +3,35 @@ package com.codepath.shopmyself.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.codepath.shopmyself.R;
+import com.codepath.shopmyself.models.CreditCard;
 import com.codepath.shopmyself.models.Item;
+import com.vinaygaba.creditcardview.CardType;
+import com.vinaygaba.creditcardview.CreditCardView;
 
 import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
 public class CheckoutActivity extends AppCompatActivity {
+
+    CreditCardView creditCardView;
+    EditText cardNameLocal;
+    EditText cardNumberLocal;
+    ImageView typeLocal;
+    ImageView brandLogoLocal;
+    ImageView chipLocal;
+    EditText expiryDateLocal;
+
+    CreditCard savedCard;
+    CheckBox cbSaveCard;
 
     ArrayList<Item> itemArrayList;
     @Override
@@ -23,5 +43,46 @@ public class CheckoutActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         itemArrayList = Parcels.unwrap(getIntent().getParcelableExtra("itemList"));
+
+        creditCardView = (CreditCardView) findViewById(R.id.card_name1);
+        cardNameLocal = (EditText) findViewById(com.vinaygaba.creditcardview.R.id.card_name);
+        cardNumberLocal = (EditText) findViewById(com.vinaygaba.creditcardview.R.id.card_number);
+        typeLocal = (ImageView) findViewById(com.vinaygaba.creditcardview.R.id.card_logo);
+        brandLogoLocal = (ImageView) findViewById(com.vinaygaba.creditcardview.R.id.brand_logo);
+        chipLocal = (ImageView) findViewById(com.vinaygaba.creditcardview.R.id.chip);
+        expiryDateLocal = (EditText) findViewById(com.vinaygaba.creditcardview.R.id.expiry_date);
+
+        savedCard = CreditCard.getInstance();
+        cbSaveCard = (CheckBox) findViewById(R.id.cbSaveCard);
+
+        if(savedCard != null) {
+            Log.d("sup:", "savedCard name: " + savedCard.getCardName());
+            creditCardView.setCardName(savedCard.getCardName());
+            creditCardView.setCardNumber(savedCard.getCardNumber());
+            creditCardView.setExpiryDate(savedCard.getCardExpiry());
+            creditCardView.setType(CardType.VISA);
+            refreshCard(creditCardView);
+        }
     }
+
+    public void saveCreditCardPay (View v) {
+
+        if(cbSaveCard.isChecked()) {
+
+            String cardNumber = creditCardView.getCardNumber();
+            String cardName = creditCardView.getCardName();
+            String expiryDate = creditCardView.getExpiryDate();
+            savedCard = new CreditCard(cardNumber, expiryDate, cardName);
+            CreditCard.setInstance(savedCard);
+            Log.d("Credit Card", "Credit Card info saved");
+            Toast.makeText(this, "Credit Card info saved" , Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void refreshCard(CreditCardView ccView) {
+        cardNameLocal.setText(ccView.getCardName());
+        cardNumberLocal.setText(ccView.getCardNumber());
+        expiryDateLocal.setText(ccView.getExpiryDate());
+    }
+
 }
